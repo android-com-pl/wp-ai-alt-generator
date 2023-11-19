976bd57162c61cc8d327bce3ee631ad832cd085f
Rafał Całka <calka.rafal@gmail.com>
2023-11-19 13:46:36 +0100

    feat: cleanup when plugin is uninstalled

---
+++ uninstall.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace ACP\AiAltGenerator;
+
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	http_response_code( 403 );
+	die;
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+delete_option( AltGeneratorPlugin::OPTION_NAME );
