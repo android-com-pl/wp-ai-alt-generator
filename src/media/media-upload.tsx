1c8adef26341b443a31b7d49bf4a29b0d6a8284b
Rafał Całka <calka.rafal@gmail.com>
2024-04-16 14:26:40 +0200

    feat: grid view mode support

--- src/media/media-upload.tsx
+++ src/media/media-upload.tsx
@@ -1,16 +1,35 @@
+import domReady from "@wordpress/dom-ready";
 import { createRoot, useEffect, useState } from "@wordpress/element";
 import { qs } from "ts-dom-utils";
 import BulkGenerateModal from "../components/BulkGenerateModal";
 import { BULK_ACTION_OPTION_VALUE } from "../constants";
-
-const reactRoot = document.createElement("div");
-reactRoot.id = "acpl-bulk-generate-alts-app";
-document.body.appendChild(reactRoot);
+import extendMediaBulkSelect from "./extend/extendMediaBulkSelect";
 
 const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMediaIds, setSelectedMediaIds] = useState<number[]>([]);
 
+  // Grid mode
+  useEffect(() => {
+    const listener = (e: CustomEvent<number[]>) => {
+      setSelectedMediaIds(e.detail);
+      setIsModalOpen(true);
+    };
+
+    document.addEventListener(
+      "triggerBulkAltGenerateModal",
+      listener as EventListener,
+    );
+
+    return () => {
+      document.removeEventListener(
+        "triggerBulkAltGenerateModal",
+        listener as EventListener,
+      );
+    };
+  }, []);
+
+  // List mode
   useEffect(() => {
     const form = qs<HTMLFormElement>("form#posts-filter");
     if (!form) return;
@@ -43,4 +62,17 @@ const App = () => {
   ) : null;
 };
 
-createRoot(reactRoot).render(<App />);
+domReady(() => {
+  extendMediaBulkSelect((ids) => {
+    document.dispatchEvent(
+      new CustomEvent("triggerBulkAltGenerateModal", {
+        detail: ids,
+      }),
+    );
+  });
+
+  const reactRoot = document.createElement("div");
+  reactRoot.id = "acpl-bulk-generate-alts-app";
+  document.body.appendChild(reactRoot);
+  createRoot(reactRoot).render(<App />);
+});
