bdef3b773a98caf2f598b6c6c31233066128fa2d
Rafał Całka <calka.rafal@gmail.com>
2024-04-20 00:43:05 +0200

    fix: use maximum available `per_page` limit

--- src/hooks/useAttachments.ts
+++ src/hooks/useAttachments.ts
@@ -1,36 +1,14 @@
-import { useSelect } from "@wordpress/data";
-import { type Attachment, store as coreStore } from "@wordpress/core-data";
+import { type Attachment, useEntityRecords } from "@wordpress/core-data";
 
 export default function useAttachments(ids: number[]) {
-  const { attachments, hasResolved } = useSelect(
-    (select) => {
-      if (!ids.length) {
-        return {
-          attachments: [],
-          hasResolved: true,
-        };
-      }
-
-      const selectorArgs = [
-        "postType",
-        "attachment",
-        { include: ids, context: "view" },
-      ] as const;
-
-      return {
-        attachments:
-          select(coreStore).getEntityRecords<Attachment<"view">>(
-            ...selectorArgs,
-          ) ?? [],
-        // @ts-ignore - missing hasFinishedResolution types
-        hasResolved: select(coreStore).hasFinishedResolution(
-          "getEntityRecords",
-          selectorArgs,
-        ),
-      };
+  const { records, hasResolved } = useEntityRecords<Attachment<"view">>(
+    "postType",
+    "attachment",
+    {
+      include: ids,
+      per_page: -1,
     },
-    [ids],
   );
 
-  return { attachments, hasResolved };
+  return { attachments: records ?? [], hasResolved };
 }
