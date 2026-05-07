d3157569148fc791e40596ff618e0a26b7199e16
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-05-07 22:16:57 +0200

    🔧 Migrate formatting from Prettier to oxfmt and reformat codebase

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
 
 module.exports = {
   ...defaultConfig,
   entry: {
-    "media-modal": "./src/media/media-modal.ts",
-    "media-edit-page": "./src/media/media-edit-page.ts",
-    "media-upload": "./src/media/media-upload.tsx",
-    editor: "./src/editor/index.tsx",
+    'media-modal': './src/media/media-modal.ts',
+    'media-edit-page': './src/media/media-edit-page.ts',
+    'media-upload': './src/media/media-upload.tsx',
+    editor: './src/editor/index.tsx',
   },
 };
