f88a3b267b2ddb0bc25166ccb3fd01968d32e3d5
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-05-26 16:28:57 +0200

    remove phpstan

--- rector.php
+++ rector.php
@@ -7,5 +7,4 @@ return RectorConfig::configure()
 	->withPaths( [ __DIR__ . '/alt-text-generator-gpt-vision.php', __DIR__ . '/includes' ] )
 	->withPhpSets()
 	->withPreparedSets( deadCode: true, codeQuality: true, typeDeclarations: true, privatization: true, earlyReturn: true )
-	->withPHPStanConfigs( [ __DIR__ . '/phpstan.neon' ] )
 	->withSkip( [ ArrayToFirstClassCallableRector::class ] );
