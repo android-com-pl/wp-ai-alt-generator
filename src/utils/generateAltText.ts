e76e2211bab38c4befdfbdef0d5016f810b5de11
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-06-16 00:59:50 +0200

    refactor generateAltText

--- src/utils/generateAltText.ts
+++ src/utils/generateAltText.ts
@@ -1,23 +1,25 @@
 import apiFetch from '@wordpress/api-fetch';
 import { GENERATE_API_PATH } from '../constants';
 
+interface Input {
+  attachment_id: number;
+  save: boolean;
+  user_prompt?: string;
+}
+
 export default async (
   attachmentId: number,
   save: boolean = false,
   userPrompt?: string,
   signal?: AbortSignal | null,
 ) => {
-  const requestData: {
-    attachment_id: number;
-    save: boolean;
-    user_prompt?: string;
-  } = {
+  const input: Input = {
     attachment_id: attachmentId,
     save,
   };
 
   if (userPrompt?.length) {
-    requestData.user_prompt = userPrompt;
+    input.user_prompt = userPrompt;
   }
 
   return apiFetch<{ alt: string; img_id: number }>({
@@ -26,7 +28,7 @@ export default async (
     headers: {
       'Content-Type': 'application/json',
     },
-    data: { input: requestData },
+    data: { input },
     signal,
   })
     .then((response) => {
